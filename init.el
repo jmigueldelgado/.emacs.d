feeedaf2dda7a784da800dc1a409b61f33a746c3
Jose Delgado <jmigueldelgado@gmail.com>
2017-11-13 09:48:27 +0100

    adds ess definitions to .init.el

--- init.el
+++ init.el
@@ -30,3 +30,31 @@
 
 ;; init.el ends here
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (material-theme better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; ESS mode configuration (only if ess is in a nonstandard place)
+(add-to-list 'load-path "/home/delgado/local/ess/share/emacs/site-lisp/ess");;<<CHANGE
+(autoload 'R-mode "ess-site.el" "ESS" t)
+(add-to-list 'auto-mode-alist '("\\.R$" . R-mode))
+(setq inferior-R-program-name "/home/delgado/local/R/bin/R");;<<CHANGE
+;;R stuff
+(setq ess-eval-visibly-p nil)
+(setq ess-ask-for-ess-directory nil)
+(require 'ess-eldoc)
+;;compile the first target in the Makefile in the current directory using F9
+(global-set-key [f9] 'compile)
+(setq compilation-read-command nil)
+;;show matching parentheses
+(show-paren-mode 1)
